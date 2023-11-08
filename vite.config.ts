8a185478d1c77803bc122e21da995317ab79d315
Alex.Cheng <1194234765@qq.com>
2023-11-07 23:13:57 -0600

    chore: vite config server proxy

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,19 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src')
+    }
+  },
+  server: {
+    cors: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8888/',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    }
+  }
 })
