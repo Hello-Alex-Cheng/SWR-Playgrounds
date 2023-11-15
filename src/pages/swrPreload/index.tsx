2c49746b8467c8b1b3b625bd272b13ad9f799193
Alex.Cheng <1194234765@qq.com>
2023-11-14 20:49:23 -0600

    feat: swr preload

--- src/pages/swrPreload/index.tsx
+++ src/pages/swrPreload/index.tsx
@@ -1,13 +1,17 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import useSWR, { preload, useSWRConfig } from 'swr'
 
+const apiKey = 'https://jsonplaceholder.typicode.com/posts/'
+
 const fetcher = (url) => fetch(url).then((res) => res.json())
-preload('https://jsonplaceholder.typicode.com/posts/', fetcher)
 
-const PreloadDemo = () => {
-	const { cache } = useSWRConfig()
+const Child = ({prefetchData}: {prefetchData: Array<any>}) => {
 
-	const { data, isValidating } = useSWR('https://jsonplaceholder.typicode.com/posts/', fetcher)
+	const { data, isValidating } = useSWR(apiKey, fetcher, {
+		revalidateOnMount: false,
+		revalidateOnFocus: false,
+    fallbackData: prefetchData
+	})
   
 	if (isValidating) {
 		return (
@@ -17,11 +21,49 @@ const PreloadDemo = () => {
 
   return (
     <div>
-			{
+      <h1>Child Component</h1>
+      {
 				data?.map(i => (
-					<p>{i.title}</p>
+					<p key={i.id}>{i.title}</p>
 				))
 			}
+    </div>
+  )
+}
+
+const PreloadDemo = () => {
+  const [show, setShow] = useState(false)
+  const [prefetchData, setPrefetchData] = useState([])
+	const config = useSWRConfig()
+
+  /** @name 直接向cache对象中写入数据  */
+  // config.cache.set('https://jsonplaceholder.typicode.com/posts/', {
+  //   data: [{ title: 123, id: 1 }],
+  //   error: false,
+  //   isValidating: false,
+  //   isLoading: false,
+  // })
+
+  const onPreloadData = async () => {
+
+    const prefetchData = await preload(apiKey, fetcher)
+
+    console.log('prefetchData ', prefetchData)
+    setPrefetchData(prefetchData)
+  }
+
+  useEffect(() => {
+    onPreloadData()
+  }, [])
+
+  return (
+    <div>
+      <h1>SWR PRELOAD</h1>
+      <p>
+        <button onClick={() => setShow(!show)}>SHOW CHILD</button>
+      </p>
+
+      {show && <Child prefetchData={prefetchData} />}
 		</div>
   )
 }
